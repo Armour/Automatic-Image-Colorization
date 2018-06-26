fixed batch normalization moving_mean and moving_variance not updated issue

common.py
@@ -83,7 +83,9 @@ def init_model(train=True):
         learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step, 100000, 0.96, staircase=True)
 
         # Optimizer
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
 
         # Summaries
         print("Init tensorflow summaries...")

read_input.py
@@ -79,7 +79,7 @@ def input_pipeline(filenames, b_size, num_epochs=None, shuffle=False, test=False
     :return: a batch of yuv_images
     """
     filename_queue = tf.train.string_input_producer(filenames, num_epochs=num_epochs, shuffle=shuffle)
-    yuv_image = read_image(filename_queue)
+    rgb_image = read_image(filename_queue)
     # min_after_dequeue defines how big a buffer we will randomly sample
     #   from -- bigger means better shuffling but slower start up and more
     #   memory used.
@@ -89,11 +89,11 @@ def input_pipeline(filenames, b_size, num_epochs=None, shuffle=False, test=False
     #   min_after_dequeue + (num_threads + a small safety margin) * batch_size
     capacity = min_after_dequeue + 3 * b_size
     if test:
-        image_batch = tf.train.batch([yuv_image],
+        image_batch = tf.train.batch([rgb_image],
                                      batch_size=b_size,
                                      capacity=capacity)
     else:
-        image_batch = tf.train.shuffle_batch([yuv_image],
+        image_batch = tf.train.shuffle_batch([rgb_image],
                                              batch_size=b_size,
                                              capacity=capacity,
                                              min_after_dequeue=min_after_dequeue)

residual_encoder.py
@@ -15,9 +15,6 @@ from config import batch_size, debug, weights, training_resize_method, tf_blur_3
 
 
 class ResidualEncoder(object):
-    def __init__(self):
-        pass
-
     @staticmethod
     def get_weight(scope):
         """
@@ -44,9 +41,9 @@ class ResidualEncoder(object):
         blur_predict_3x3 = tf.nn.conv2d(predict_val, tf_blur_3x3, strides=[1, 1, 1, 1], padding='SAME', name="blur_predict_3x3")
         blur_predict_5x5 = tf.nn.conv2d(predict_val, tf_blur_5x5, strides=[1, 1, 1, 1], padding='SAME', name="blur_predict_5x5")
 
-        diff_original = tf.reduce_sum(tf.abs(tf.subtract(predict_val, real_val)), name="diff_original")
-        diff_blur_3x3 = tf.reduce_sum(tf.abs(tf.subtract(blur_predict_3x3, blur_real_3x3)), name="diff_blur_3x3")
-        diff_blur_5x5 = tf.reduce_sum(tf.abs(tf.subtract(blur_predict_5x5, blur_real_5x5)), name="diff_blur_5x5")
+        diff_original = tf.abs(tf.subtract(predict_val, real_val), name="diff_original")
+        diff_blur_3x3 = tf.abs(tf.subtract(blur_predict_3x3, blur_real_3x3), name="diff_blur_3x3")
+        diff_blur_5x5 = tf.abs(tf.subtract(blur_predict_5x5, blur_real_5x5), name="diff_blur_5x5")
         return tf.div(tf.add_n([diff_original, diff_blur_3x3, diff_blur_5x5]), 3)
 
     @staticmethod
@@ -58,10 +55,7 @@ class ResidualEncoder(object):
         :param training_flag: the flag indicate if it is training
         :return: normalized data
         """
-        return tf.cond(training_flag,
-                       lambda: tf.layers.batch_normalization(input_data, training=True, name=scope),
-                       lambda: tf.layers.batch_normalization(input_data, training=False, name=scope, reuse=True),
-                       name='batch_normalization')
+        return tf.layers.batch_normalization(input_data, training=training_flag, name=scope)
 
     def conv_layer(self, layer_input, scope, is_training, relu=True, bn=True):
         """