b79edc8cfff01a83669223794b0b7b9b74b43d73
Misal <misal@gmail.com>
2019-08-11 18:12:47 +0530

    corrected model loading , with allow_pickle=True and modified test dir to point to correct directory

--- vgg/vgg16.py
+++ vgg/vgg16.py
@@ -20,7 +20,7 @@ class Vgg16:
             vgg16_npy_path = path
             print(path)
 
-        self.data_dict = np.load(vgg16_npy_path, encoding='latin1').item()
+        self.data_dict = np.load(vgg16_npy_path, encoding='latin1', allow_pickle=True).item()
         print("npy file loaded")
 
     def build(self, rgb):
